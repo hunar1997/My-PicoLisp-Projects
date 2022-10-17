0153dae43ec10380ed9a4af5770db316091381ff
hunar1997 <hbkurd@gmail.com>
2022-10-18 00:03:41 +0300

    Trying to add a test
    Also made more progress in the python file

--- python-from-pil/general.py
+++ python-from-pil/general.py
@@ -3,14 +3,27 @@ import re
 def line_split(line):
         return re.findall(r'[^"\s]\S*|".+?"', line) # magic from the internet
 
+def to_lisp(L,initial="'"):
+    if type(L)==list:
+        return initial + "(" + " ".join([to_lisp(i,"") for i in L]) + ")"
+    elif type(L)==str:
+        return "\"" + L + "\""
+    else:
+        return L
+    # todo: integer is fine, but float maybe scientific noation
+
+def from_lisp(L):
+    return L
+
 code = input()[1:-1]
 if code[0]!="'":
     code=code.split()
-    print(eval(( f"{code[0]}({', '.join(code[1:]) })" )))
+    print(eval( f"{code[0]}({', '.join(code[1:]) })" ))
 else:
     end=code.find(')')
     dots = ".".join(line_split(code[2:end]))
     params = ", ".join(line_split(code[end+1:]))
-    print(eval(f"{dots}({params})"))
+    res = eval(f"{from_lisp(dots)}({from_lisp(params)})")
+    print( to_lisp(res) )
 
 
